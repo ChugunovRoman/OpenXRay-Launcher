d23a720058e2f0321ed3db3b85e00dfff6204a1a
Chugunov Roman <Zebs-BMK@yandex.ru>
2022-12-05 20:35:09 +0300

    set CanResize on the main window to false

--- src/App.axaml.cs
+++ src/App.axaml.cs
@@ -18,6 +18,7 @@ public partial class App : Application
     if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
     {
       desktop.MainWindow = new MainWindow();
+      desktop.MainWindow.CanResize = false;
     }
 
     base.OnFrameworkInitializationCompleted();
